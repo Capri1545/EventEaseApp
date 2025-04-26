03da245a7685aab132a149991ab1528dfadfc79f
Capri1545 <chancecapri@hotmail.com>
2025-04-26 10:39:05 -0700

    Add compressed data file for net9.0 build

--- obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/EventEaseApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91b98d07218660017889b3b9af401c420eb27417")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventEaseApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
